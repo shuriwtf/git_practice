0c41fb1c7a2cf14fb02852bd5ec1897ba11ae2b0
shuriwtf <tmcuong0112@gmail.com>
2020-04-27 23:35:05 +0700

    initial commit

---
+++ script.py
@@ -0,0 +1,6 @@
+destinations = ['Paris, France'
+'Shanghai, China'
+'Los Angeles, USA'
+'São Paulo, Brazi'
+'Cairo, Egypt']
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
